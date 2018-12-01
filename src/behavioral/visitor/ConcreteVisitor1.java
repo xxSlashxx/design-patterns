a44a8a079b259b3fa27b3ce375e73050ac1f54c1
xxSlashxx <roland.speckmeier@gmx.de>
2018-12-01 18:34:47 +0100

    Added visitor pattern

--- src/behavioral/visitor/ConcreteVisitor1.java
+++ src/behavioral/visitor/ConcreteVisitor1.java
@@ -5,12 +5,12 @@ public class ConcreteVisitor1 implements IVisitor
     @Override
     public void visit(ConcreteElementA a)
     {
-        System.out.println("visit ConcreteElementA");
+        System.out.println("visit ConcreteElementA in ConcreteVisitor1");
     }
 
     @Override
     public void visit(ConcreteElementB b)
     {
-        System.out.println("visit ConcreteElementB");
+        System.out.println("visit ConcreteElementB in ConcreteVisitor1");
     }
 }
--- src/behavioral/visitor/ConcreteVisitor2.java
+++ src/behavioral/visitor/ConcreteVisitor2.java
@@ -5,12 +5,12 @@ public class ConcreteVisitor2 implements IVisitor
     @Override
     public void visit(ConcreteElementA a)
     {
-
+        System.out.println("visit ConcreteElementA in ConcreteVisitor2");
     }
 
     @Override
     public void visit(ConcreteElementB b)
     {
-
+        System.out.println("visit ConcreteElementA in ConcreteVisitor2");
     }
 }
